39ea75067bddb69fbfaeca1ef2e28945a81273fe
Scott Reoch <scottreoch10@hotmail.co.uk>
2022-03-01 20:05:56 +0000

    added some styling and links

--- client/src/components/SideBar/index.jsx
+++ client/src/components/SideBar/index.jsx
@@ -1,17 +1,15 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 
 const SideBar = () => {
+
   return (
-    <div className="fixed h-full mt-16 t-0 w-44 flex flex-col bg-white dark:bg-gray-900 shadow-lg">
-                    
-        <SideBarIcon icon={<FaFire size="28" />} />
-        <Divider />
-        <SideBarIcon icon={<BsPlus size="32" />} />
-        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-        <SideBarIcon icon={<FaPoo size="20" />} />
-        <Divider />
-        <SideBarIcon icon={<BsGearFill size="22" />} />
+    <div className="font-zappr text-xl text-blue-300 items-center bg-pink-400 fixed h-full mt-16 pt-4 t-0 w-44 flex flex-col dark:bg-gray-900 dark:text-yellow-300 shadow-lg">
+      <div className=''>
+      <Link to="/">Dashboard</Link>
+      </div>
+      <Divider />
+      <Link to="/bugs">Bug Table</Link>
     </div>
   );
 };
